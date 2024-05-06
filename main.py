a649f389462c54a5c065ac012696fca2ff6adbde
Jasmine Lopez <Jas@elias.local>
2024-05-06 00:01:29 -0600

    Finished choosing best model

--- main.py
+++ main.py
@@ -138,6 +138,15 @@ def go(config: DictConfig):
             # Implement here #
             ##################
 
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/test_regression_model",
+                'main',
+                parameters = {
+                    "mlflow_model": "random_forest_export:prod",
+                    "test_dataset": "test_data.csv:latest"
+                    
+                }
+            )
             pass
 
 
